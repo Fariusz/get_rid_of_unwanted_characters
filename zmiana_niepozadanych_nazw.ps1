2ebf2c3f5c66c0941f0b1dbaa6b06fa76d6a413d
Radosław <61850108+Fariusz@users.noreply.github.com>
2020-09-30 14:45:43 +0200

    Add files via upload

---
+++ zmiana_niepozadanych_nazw.ps1
@@ -0,0 +1,21 @@
+﻿#SKRYPT: Oczyszczanie nazw plików i folderów ze spacji oraz polskich znaków
+
+#Tablica znaków niepożądanych
+$zleZnaki = @(' ','ą','ć','ę','ł','ń','ó','ś','ź','ż','(',')','[',']','{','}')
+
+#Tablica znaków pożądanych
+$dobreZnaki = @('_','a','c','e','l','n','o','s','z','z','_','','_','','_','')
+
+for ($i=0; $zleZnaki.Count -gt $i; $i++)
+{
+    #Ścieżka do folderu nadrzędnego z flagą Recurse pozwalającą na przeszukiwanie wgłąb hierarchi folderów
+    Get-ChildItem -Path .\ -Recurse |
+    Where-Object { $_.name.Contains($zleZnaki[$i]) } |
+    Rename-Item -NewName { $_.name -replace [regex]::Escape($zleZnaki[$i]),$dobreZnaki[$i] }
+}
+
+
+
+
+
+
